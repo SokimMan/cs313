dd7c2d239d184885ce5e78ec3b1ffe2d919fe30c
Gage <35212618+SokimMan@users.noreply.github.com>
2020-05-31 22:56:59 -0600

    php db file

---
+++ web/ponder05.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+
+foreach ($db->query('SELECT , firstname, lastname FROM salesforce') as $row)
+{
+  echo 'salesforce: ' . $row['firstname'];
+  echo ' dynamics: ' . $row['lastname'];
+  echo '<br/>';
+}
+
+</body>
+</html>
