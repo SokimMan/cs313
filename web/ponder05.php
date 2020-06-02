a62e076ebc1c4b7db62cfbc4b5e4326bf29e1087
Gage <35212618+SokimMan@users.noreply.github.com>
2020-06-02 14:31:59 -0600

    w

--- web/ponder05.php
+++ web/ponder05.php
@@ -24,6 +24,7 @@ try
   $dbName = ltrim($dbOpts["path"],'/');
 
   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+  echo $db;
 
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
