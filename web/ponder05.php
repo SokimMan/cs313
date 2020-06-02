82873b9e3cb25c5cbba498071d89605f722f8149
Gage <35212618+SokimMan@users.noreply.github.com>
2020-06-02 12:59:38 -0600

    work

--- web/ponder05.php
+++ web/ponder05.php
@@ -1,12 +1,18 @@
-
 <!DOCTYPE html>
 <html>
 <body>
 
+
 <?php
 try
 {
+  
   $dbUrl = getenv('DATABASE_URL');
+
+  if (empty($dbUrl)) {
+  // example localhost configuration URL with postgres username and a database called cs313db
+  $dbUrl = "postgres://postgres:password@localhost:5432/db";
+  }
 
   $dbOpts = parse_url($dbUrl);
 
